bcd144825c17c4c863178a94615f278a452ac9f7
Michael Legleux <legleux@protonmail.com>
2022-11-29 00:34:39 -0800

    fixed

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ def check_releases_needed(repo=SOURCE_REPO, git_rev=None):
     source_repo_latest_commits = get_last_commits_from_target_branches()
     releases = get_releases(repo)
     released_tags = [ release['tag_name'] for release in releases ]
-
+    releases_found = []
     for latest_commit in source_repo_latest_commits:
         branch, git_rev = latest_commit
         release_tag = f"{branch}_{git_rev[:8]}"
+
         if release_tag not in released_tags:
             # log this somehow write a file and read it to markdown for display in github?
             # print(f"need a release for {latest_commit[0]} -- {release_tag}")
-            print(f"{branch}-{git_rev}")
-            exit()
+            releases_found.append(release_tag)
         else:
+            pass
             # print(f"Found a release for {latest_commit[0]} -- {release_tag}")
-            print("false")
-            return False
+    if not releases_found:
+        print("false")
+    else:
+        print(releases_found[0])
 
 check_releases_needed()
