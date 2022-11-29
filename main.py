ff95866a6315d5104b5ba66d429b325c44376e3a
Michael Legleux <legleux@protonmail.com>
2022-11-28 23:38:42 -0800

    check which source branches don't have releases in target repo

--- main.py
+++ main.py
@@ -1,39 +1,23 @@
 # get head commits of branches
-from check_gh_latest_commits import get_branch_sha, get_hash_from_tag
+from check_gh_latest_commits import get_last_commits_from_target_branches, get_hash_from_tag
 from check_releases import get_releases
-
-# get latest commits from source repo, "clio"
-latest_commits = get_branch_sha()
 
 REPO = "legleux/package-syncer"
 
-def check_if_release_needed(repo=REPO, git_rev=None):
-    hash_needs_release = True
-# check if pkg-sync repo has a release has a release for each
-    releases = get_releases(repo)
-    # for release in releases:
-    #     print(release['tag_name'])
-    #     print(release['published_at'])
+def check_releases_needed(repo=REPO, git_rev=None):
 
-    releases = [ release['tag_name'] for release in releases ]
-    # released_shas = [ sha[1] for sha in releases if len(sha.split('-')) > 1]
-    # TODO: figure out what to name releases
-    released_shas = [ sha for sha in releases]
-    for commit in latest_commits:
-        commit = commit[1][:8]
-        if not commit in released_shas:
-            # print(f"Need to release: {commit}")
-            print(commit[1])
-            return commit[1]
+    source_repo_latest_commits = get_last_commits_from_target_branches()
+
+    releases = get_releases(repo)
+    released_tags = [ release['tag_name'] for release in releases ]
+
+    for latest_commit in source_repo_latest_commits:
+        release_tag = f"{latest_commit[0]}-{latest_commit[1][:8]}"
+        if release_tag not in released_tags:
+            # log print(f"need a release for {latest_commit[0]} -- {release_tag}")
+            print(latest_commit[1])
         else:
             print("false")
             return False
-    # released_shas = [ get_hash_from_tag(release) for release in releases ]
-    # breakpoint()
-    # for commit in latest_commits:
-        # sha = commit[1]
-        # breakpoint()
-        # if get_hash_from_tag(tag)
-        # print()
 
-check_if_release_needed()
+check_releases_needed()
