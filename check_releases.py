f9385617196f6b72d907676b079520aadffdf198
Michael Legleux <mlegleux@ripple.com>
2022-11-29 23:44:14 -0800

    refact

--- check_releases.py
+++ check_releases.py
@@ -2,24 +2,22 @@ from requests import post, get
 import json
 import os
 from dotenv import dotenv_values
-OWNER = "legleux"
-REPOSITORY = "test_repo"
-# OWNER = "XRPLF"
-# REPOSITORY = "clio"
-REPO = f"{OWNER}/{REPOSITORY}"
-URL = f"https://api.github.com/repos/{REPO}/actions/artifacts"
-if os.environ.get('CI'):
+
+if os.environ.get('GITHUB_ACTIONS'):
   TOKEN = os.environ['PAT_TOKEN']
 else:
   TOKEN = dotenv_values().get('PAT_TOKEN')
 
-def get_releases(repo=REPO):
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {TOKEN}"
+}
 
-    headers = {
-        "Accept": "application/vnd.github+json",
-       "Authorization": f"Bearer {TOKEN}"
-    }
-    URL = f"https://api.github.com/repos/{repo}/releases"
-    resp = json.loads(get(URL, headers=headers).content)
-    # breakpoint()
-    return resp
+def get_gh_releases(repo):
+    url = f"https://api.github.com/repos/{repo}/releases"
+    releases = json.loads(get(url, headers=headers).content)
+    return releases
+
+#for each release in package-syncer releases, there must be a corresponding package in artifactory:
+
+branch_map = {"nightly":"develop", "unstable":"release", "stable": "main"}
