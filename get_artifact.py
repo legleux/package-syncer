6a19577705b2b99473d659ff430c58d379cf78aa
Michael Legleux <legleux@protonmail.com>
2022-11-30 00:46:54 -0800

    make summary page

--- get_artifact.py
+++ get_artifact.py
@@ -1,5 +1,5 @@
 from requests import post, get
-import json
+import json, re
 import subprocess as sp
 import os
 import sys
@@ -10,7 +10,7 @@ OWNER = "legleux"
 REPO = "test_repo"
 URL = f"https://api.github.com/repos/{OWNER}/{REPO}/actions/artifacts"
 
-if os.environ.get('CI'):
+if os.environ.get('GITHUB_ACTIONS'):
   TOKEN = os.environ['PAT_TOKEN']
 else:
   TOKEN = dotenv_values().get('PAT_TOKEN')
@@ -23,7 +23,7 @@ headers = {
 def get_artifacts():
   resp = json.loads(get(URL, headers=headers).content)
   artifacts = resp['artifacts'][0]
-  print(artifacts['workflow_run']['head_sha'])
+  # print(artifacts['workflow_run']['head_sha'])
   dl_url = artifacts['archive_download_url']
   wf_url = artifacts['url']
   r = get(dl_url, headers=headers, allow_redirects=True)
@@ -34,8 +34,8 @@ def get_latest_artifact_urls(git_rev, branch):
 
   a = [ a for a in artifacts if a['workflow_run']['head_sha'] == git_rev and a['workflow_run']['head_branch'] == branch]
   # breakpoint()
-  for i in a:
-    print(i['archive_download_url'])
+  # for i in a:
+  #   print(i['archive_download_url'])
   return a
 
 def download_artifact(artifact_json):
@@ -47,13 +47,27 @@ def download_artifact(artifact_json):
     zip_file.write(r.content)
     zip_file.close()
     o = sp.check_output(['unzip', name])
-    print(o.decode())
-    # o = sp.check_output(['sha256sum', 'cool.deb'])
-    # sha = o.decode().split(' ')[0]
-    # print(sha)
+    zip_output = o.decode()
+    # breakpoint()
+    # archive_name = re.search("Archive: *(.*)\\n", zip_output)[1]
+    artifact = re.search("inflating: (\S*)", zip_output)[1]
+    # return the filename and sha256s
+
+    # sha_output = sp.check_output(['sha256sum', artifact_name])
+    # sha = sha_output.decode().split(' ')[0]
+    ## or
+    sha = name.split('-')[2]
+    return (artifact, sha)
+
 
 git_rev = sys.argv[1]
 branch = sys.argv[2]
 artifacts = get_latest_artifact_urls(git_rev, branch)
+pkg_shas = []
+
 for art in artifacts:
-  download_artifact(art)
+  pkg_shas.append(download_artifact(art))
+
+for info in pkg_shas:
+  print(f"File: {info[0]}\sha256: {info[1]}")
+  print("----")
